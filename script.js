e07f6abf2f0d30fb8c00ae1f31af0d32c4dee738
argumpamungkas <argumpamungkaass@gmail.com>
2023-05-10 11:13:13 +0700

    update animasi

--- script.js
+++ script.js
@@ -23,3 +23,55 @@ form.addEventListener("submit", (e) => {
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+// AOS
+
+const galImg = Array.from(document.querySelectorAll(".gallery-img"));
+
+galImg.forEach((img, i) => {
+  img.dataset.aos = "fade-down";
+  img.dataset.aosDelay = i * 200;
+  img.dataset.aosDuration = 1000;
+});
+
+AOS.init({
+  duration: 1000,
+});
+
+// GSAP
+gsap.registerPlugin(TextPlugin);
+gsap.from(".navbar", {
+  duration: 3,
+  opacity: 0,
+  ease: "back",
+});
+gsap.from(".display-4", {
+  duration: 2,
+  delay: 0.5,
+  opacity: 0,
+  ease: "back",
+});
+
+gsap.to(".lead", {
+  duration: 1.5,
+  delay: 1,
+  text: "Android Developer",
+});
+
+gsap.to(".lead", {
+  duration: 1.5,
+  delay: 3,
+  text: "Frontend Developer",
+});
+
+gsap.to(".lead", {
+  duration: 1.5,
+  delay: 5,
+  text: "Backend Developer",
+});
+
+gsap.to(".lead", {
+  duration: 1.5,
+  delay: 7,
+  text: "Software Developer",
+});
